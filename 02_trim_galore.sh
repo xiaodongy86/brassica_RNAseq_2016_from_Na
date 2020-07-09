6cf61dc260c7f99987ebf41d2d1b937d13821877
xiaodong yang <xiaodongy86@gmail.com>
2020-07-09 10:45:53 -0400

    Create 02_trim_galore.sh

---
+++ 02_trim_galore.sh
@@ -0,0 +1,11 @@
+ts /usr/local/bin05169/trim_galore --phred33 --gzip --fastqc --length 30 -o /data5/xzy50/brassica/RNAseq_2016/trim_galore/CMS_L_NP_1 /data5/xzy50/brassica/RNAseq_2016/CMS_L_NP_1/0CL300001525_L01_12_1.fq.gz
+
+ts /usr/local/bin05169/trim_galore --phred33 --gzip --fastqc --length 30 -o /data5/xzy50/brassica/RNAseq_2016/trim_galore/CMS_L_NP_2 /data5/xzy50/brassica/RNAseq_2016/CMS_L_NP_2/0CL300001525_L01_13_1.fq.gz 
+
+ts /usr/local/bin05169/trim_galore --phred33 --gzip --fastqc --length 30 -o /data5/xzy50/brassica/RNAseq_2016/trim_galore/CMS_L_NP_3 /data5/xzy50/brassica/RNAseq_2016/CMS_L_NP_3/0CL300001525_L01_14_1.fq.gz
+
+ts /usr/local/bin05169/trim_galore --phred33 --gzip --fastqc --length 30 -o /data5/xzy50/brassica/RNAseq_2016/trim_galore/CMS_L_P_1 /data5/xzy50/brassica/RNAseq_2016/CMS_L_P_1/0CL300001525_L01_15_1.fq.gz
+
+ts /usr/local/bin05169/trim_galore --phred33 --gzip --fastqc --length 30 -o /data5/xzy50/brassica/RNAseq_2016/trim_galore/CMS_L_P_2 /data5/xzy50/brassica/RNAseq_2016/CMS_L_P_2/0CL300001525_L01_16_1.fq.gz
+
+ts /usr/local/bin05169/trim_galore --phred33 --gzip --fastqc --length 30 -o /data5/xzy50/brassica/RNAseq_2016/trim_galore/CMS_L_P_3 /data5/xzy50/brassica/RNAseq_2016/CMS_L_P_3/0CL300001525_L01_17_1.fq.gz
