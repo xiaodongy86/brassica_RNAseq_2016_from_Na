24be6101631be2f27e4038385fd281cd1a487017
xiaodong yang <xiaodongy86@gmail.com>
2020-07-09 21:06:03 -0400

    Update 03_star_index.sh

--- 03_star_index.sh
+++ 03_star_index.sh
@@ -1,7 +1,16 @@
+# download Brassica_juncea genome and annotation files
+wget http://brassicadb.org/brad/datasets/pub/Genomes/Brassica_juncea/V1.5/Bju.chr.modified_id.cds.fa.gz
+wget http://brassicadb.org/brad/datasets/pub/Genomes/Brassica_juncea/V1.5/Bju.chr.modified_id.pep.fa.gz
+wget http://brassicadb.org/brad/datasets/pub/Genomes/Brassica_juncea/V1.5/Bju.genome.fasta.gz
+wget http://brassicadb.org/brad/datasets/pub/Genomes/Brassica_juncea/V1.5/Bju.genome.gff.gz
+
+#convert gff to gft using AGAT
+agat_convert_sp_gff2gtf.pl --gff Bju.genome.gff -o Bju.genome.gtf
+
 THREADS=6
 GENOME_INDEX_DIR=/data5/xzy50/brassica/RNAseq_2016/Genome
 FASTA_FILES=/data5/xzy50/brassica/RNAseq_2016/Genome/Bju.genome.fasta
-GTF_FILE=/data5/xzy50/brassica/RNAseq_2016/Genome/Bju.genome.gff
+GTF_FILE=/data5/xzy50/brassica/RNAseq_2016/Genome/Bju.genome.gtf
 ulimit -n 2048
 #mkdir -p ${GENOME_INDEX_DIR}
 #STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE} 
@@ -11,4 +20,4 @@ ulimit -n 2048
 #          If exons are marked with a different word, use --sjdbGTFfeatureExon .
 
 # the name of exon is mRNA
-STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE} --sjdbGTFfeatureExon mRNA
+STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE} 
